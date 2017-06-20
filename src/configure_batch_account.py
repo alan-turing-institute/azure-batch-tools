d0c0f95a825794a792d4aa77cae74599cb85f477
Martin O'Reilly <developer@mtspace.net>
2017-06-20 11:15:24 +0100

    Create batch account with SP user

--- src/configure_batch_account.py
+++ src/configure_batch_account.py
@@ -5,6 +5,8 @@ import string
 import random
 import uuid
 import argparse
+import time
+import subprocess
 
 from azure.cli.core.application import APPLICATION, Configuration
 from azure.cli.core._session import ACCOUNT, CONFIG, SESSION
@@ -27,6 +29,8 @@ AZURE_PASSWORD_CHARSET = string.ascii_lowercase + string.ascii_uppercase + strin
 DEFAULT_LOCATION = 'westeurope'
 DEFAULT_STORAGE_SKU = 'Standard_LRS'
 
+DEFAULT_SSH_KEY_DIRECTORY = "private-batch-ssh-keys"
+
 # Set up some exit statuses
 CLEAN_EXIT = 0
 USER_EXIT = 1
@@ -36,6 +40,9 @@ def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser(description=__name__)
     parser.add_argument('command', choices=['create'])
+    parser.add_argument('--subscription','-s',
+        required=True,
+        help='Name or ID of subscription to create resources under. NOTE: Your default subscription will be changed to the one specified here for the duration of your logged in Azure CLI session.')
     parser.add_argument('--name','-n',
         required=True,
         help='Name of batch account. If creating a new batch account, this will also be used as the name of the associated resource group and storage account that will be created.')
@@ -70,15 +77,29 @@ def main():
     # default subscription, use set_default_subscription(name_or_id). This
     # changes the default subscription for this session only.
     # TODO: Take subscription as a commandline argument
-    subscription = get_default_subscription()
-    logger.warning("Using default subscription ({0} / {1})".format(subscription["name"], subscription["id"]))
+
+    subscription = set_subscription(args.subscription)
 
     if(args.command == 'create'):
-        create(args)
+        create(args, subscription)
     else:
-        warning("Unsupported command")
+        logger.warning("Unsupported command")
 
-def create(args):
+def set_subscription(subscription_name_or_id):
+    subscription = get_default_subscription()
+    if((subscription["name"] == subscription_name_or_id or  subscription["id"] == subscription_name_or_id)):
+        logger.warning("Default subscription already set to {0} ({1})".format(subscription["name"], subscription["id"]))
+    else:
+        subscription_opt = "--subscription={:s}".format(subscription_name_or_id)
+        try:
+            subscription = APPLICATION.execute(['account', 'set', subscription_opt]).result
+        except Exception as e:
+            logger.error("Cannot change subscription to '{:s}': {:s}".format(subscription_name_or_id, e.args[0]))
+            sys.exit()
+    return(subscription)
+
+
+def create(args, subscription):
     # Generate unique ID to use for resource group, batch account, storage
     # account names
     # TODO: Potentially tke account name as a command line argument? If so,
@@ -88,20 +109,72 @@ def create(args):
     # Check name
     if(name_valid(account_name)):
         # Create resource group, batch account and storage account
-        create_batch_account_group(args.name)
+        create_batch_account_group(args.name, subscription)
+
+def ensure_resource_provider_registered(namespace):
+    if(resource_provider_registration_state(namespace) == "Registered"):
+        return
+    if(resource_provider_registration_state(namespace) == "Unregistering"):
+        while (resource_provider_registration_state(namespace) == "Unregistering"):
+            print("Waiting for earlier deregistration of resource provider '{:s}' to complete before re-registering.".format(namespace))
+            time.sleep(5)
+    if(resource_provider_registration_state(namespace) == "Unregistered"):
+        register_resource_provider(namespace)
+        while (resource_provider_registration_state(namespace) == "Registering"):
+            print("Waiting for registration of resource provider '{:s}' to complete.".format(namespace))
+            time.sleep(5)
+    return
+
+def resource_provider_registration_state(namespace):
+    name_opt = "--name={0}".format(namespace)
+    return APPLICATION.execute(['provider', 'show', name_opt]).result["registrationState"]
+
+def resource_provider_registered(namespace):
+    state = resource_provider_registration_state(namespace)
+    return (state == "Registered")
+
+def register_resource_provider(namespace):
+    name_opt = "--name={0}".format(namespace)
+    APPLICATION.execute(['provider', 'register', name_opt]).result
+
+def ensure_exists(directory):
+    if(directory and not os.path.exists(directory)):
+        os.makedirs(directory)
+
+def create_service_principle_for_resource_group(service_principle_name, subscription, resource_group_name):
+    name_opt = "--name={0}".format(service_principle_name)
+    cert_opt = "--create-cert"
+    role_opt = "--role=Contributor"
+    scopes_opt = "--scope=/subscriptions/{:s}/resourceGroups/{:s}".format(subscription["id"], resource_group_name)
+    # Check if user already ensure_exists
+    id_opt = "--id=http://{0}".format(service_principle_name)
+    try:
+        res = APPLICATION.execute(['ad', 'sp', 'show', id_opt]).result
+        # If no error then lookup was successful and user exists
+        logger.warning("Batch service principle user {0} already exists. Skipping create.".format(service_principle_name))
+    except Exception as e:
+        logger.warning("Creating batch service principle user")
+        res = APPLICATION.execute(['ad', 'sp', 'create-for-rbac', cert_opt, name_opt, role_opt]).result
+        pem_source_path = res["fileWithCertAndPrivateKey"]
+        ensure_exists(DEFAULT_SSH_KEY_DIRECTORY)
+        pem_dest_filename = "{:s}.pem".format(resource_group_name)
+        pem_dest_path = os.path.join(DEFAULT_SSH_KEY_DIRECTORY, pem_dest_filename)
+        subprocess.call(["mv", pem_source_path, pem_dest_path], stderr=subprocess.STDOUT)
 
 def create_batch_account(name, resource_group_name, location = DEFAULT_LOCATION):
     logger.warning("Creating batch account")
+    ensure_resource_provider_registered("Microsoft.Batch")
     name_opt = "--name={0}".format(name)
     location_opt = "--location={0}".format(location)
     resource_group_name_opt = "--resource-group={0}".format(resource_group_name)
     return APPLICATION.execute(['batch', 'account', 'create', name_opt, location_opt, resource_group_name_opt]).result
 
-def create_batch_account_group(name, location = DEFAULT_LOCATION):
+def create_batch_account_group(name, subscription, location = DEFAULT_LOCATION):
     logger.warning("Creating resource group, batch account and storage account with name '{0}'".format(name))
     resource_group = create_resource_group(name, location)
     print("Resource group: {0}".format(resource_group))
     resource_group_name = resource_group["name"]
+    create_service_principle_for_resource_group(name, subscription, resource_group_name)
     batch_account = create_batch_account(name, resource_group_name, location)
     print("Batch account: {0}".format(batch_account))
     storage_account = create_storage_account(name, resource_group_name, location)
